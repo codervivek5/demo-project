c773e252f21f3d9cd4040ad40112bf6f12f30059
codervivek <62784060+codervivek5@users.noreply.github.com>
2023-02-20 23:07:13 +0530

    added some more files in our android project

---
+++ app/src/main/java/com/example/formfil/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.formfil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        getSupportActionBar().hide();
+    }
+}
